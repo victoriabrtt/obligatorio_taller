d573904a8dfe46842ec8b2e9aa046c949c08ce4d
Victoria <victoriabartolotta108@gmail.com>
2025-06-07 15:58:50 -0300

    added staking functionalities

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -18,6 +18,15 @@ contract DAO {
     uint256 public proposalDurationDays;
     uint256 public tokenPriceInWei;
 
+    struct StakeInfo {
+        uint256 amount;
+        uint256 timestamp;
+    }
+
+    mapping(address => StakeInfo) public voteStakes;
+    mapping(address => StakeInfo) public proposalStakes;
+
+
     modifier onlyOwner() {
         require(msg.sender == ownerMultisig, "Not owner");
         _;
@@ -70,5 +79,39 @@ contract DAO {
 
     function tranquility() external onlyPanic {
         isPaused = false;
+    }
+
+    function stakeForVote(uint256 amount) external daoActive {
+        require(amount >= stakingToVote, "Insufficient staking amount");
+        require(voteStakes[msg.sender].amount == 0, "Already staked");
+
+        token.transferFrom(msg.sender, address(this), amount);
+        voteStakes[msg.sender] = StakeInfo(amount, block.timestamp);
+    }
+
+    function stakeForProposal(uint256 amount) external daoActive {
+        require(amount >= stakingToPropose, "Insufficient staking amount");
+        require(proposalStakes[msg.sender].amount == 0, "Already staked");
+
+        token.transferFrom(msg.sender, address(this), amount);
+        proposalStakes[msg.sender] = StakeInfo(amount, block.timestamp);
+    }
+
+    function unstakeVote() external {
+        StakeInfo memory stake = voteStakes[msg.sender];
+        require(stake.amount > 0, "No tokens staked");
+        require(block.timestamp >= stake.timestamp + minStakingTime, "Staking time not met");
+
+        delete voteStakes[msg.sender];
+        token.transfer(msg.sender, stake.amount);
+    }
+
+    function unstakeProposal() external {
+        StakeInfo memory stake = proposalStakes[msg.sender];
+        require(stake.amount > 0, "No tokens staked");
+        require(block.timestamp >= stake.timestamp + minStakingTime, "Staking time not met");
+
+        delete proposalStakes[msg.sender];
+        token.transfer(msg.sender, stake.amount);
     }
 }
