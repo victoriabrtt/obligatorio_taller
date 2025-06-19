9cf2f8998c65d8cbaad93e1aef91a9225fa1fd55
Juan <jc314750@fi365.ort.edu.uy>
2025-06-19 10:06:48 -0300

    Reduccion_ DashboardPage

--- frontend/dao-frontend/src/App.tsx
+++ frontend/dao-frontend/src/App.tsx
@@ -7,8 +7,6 @@ import ProposalsList from './pages/ProposalsList';
 import StakingPage from './pages/StakingPage';
 import CreateProposal from './pages/CreateProposal';
 import ProposalDetail from './pages/ProposalDetail';
-import DelegationPage from './pages/DelegationPage';
-import DashboardPage from './pages/DashboardPage';
 
 function App() {
   return (
@@ -18,13 +16,11 @@ function App() {
           <Header />
           <Container maxW="container.xl" pt={4}>
             <Routes>
-              <Route path="/" element={<DashboardPage />} />
-              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route path="/" element={<ProposalsList />} />
               <Route path="/proposals" element={<ProposalsList />} />
               <Route path="/proposals/create" element={<CreateProposal />} />
               <Route path="/proposals/:id" element={<ProposalDetail />} />
               <Route path="/staking" element={<StakingPage />} />
-              <Route path="/delegation" element={<DelegationPage />} />
             </Routes>
           </Container>
         </Box>
--- frontend/dao-frontend/src/components/Header.tsx
+++ frontend/dao-frontend/src/components/Header.tsx
@@ -20,11 +20,9 @@ const Header: React.FC = () => {
       
       <Flex align="center">
         <Flex mr={8} gap={6}>
-          <ChakraLink as={RouterLink} to="/" _hover={{ color: 'blue.100' }}>Dashboard</ChakraLink>
           <ChakraLink as={RouterLink} to="/proposals" _hover={{ color: 'blue.100' }}>Propuestas</ChakraLink>
           <ChakraLink as={RouterLink} to="/staking" _hover={{ color: 'blue.100' }}>Staking</ChakraLink>
           <ChakraLink as={RouterLink} to="/proposals/create" _hover={{ color: 'blue.100' }}>Crear Propuesta</ChakraLink>
-          <ChakraLink as={RouterLink} to="/delegation" _hover={{ color: 'blue.100' }}>Delegación</ChakraLink>
         </Flex>
         
         <Text mr={4} fontSize="sm">
